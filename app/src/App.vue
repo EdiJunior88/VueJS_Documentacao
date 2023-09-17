4510a1c4b2acd3960c49167b102dbdd8691801b5
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-16 21:52:32 -0300

    - Começar Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,32 @@
 <script>
 export default {
+  // As propriedades retornadas de `data()` tornam-se estados reativos
+  // e serão expostas no `this`.
   data() {
     return {
       count: 0,
     };
   },
+
+  // Os métodos são funções que alteram o estado e acionam atualizações.
+  // Elas podem ser atadas como ouvintes de evento no modelos de marcação.
+  methods: {
+    increment() {
+      this.count++;
+    },
+  },
+
+  // Gatilhos do ciclo de vida são chamados em diferentes fases
+  // de um ciclo de vida do componente.
+  // Esta função será chamada quando o componente for montado.
+  mounted() {
+    console.log(`The initial count is ${this.count}`);
+  },
 };
 </script>
 
 <template>
-  <button @click="count++">Count is: {{ count }}</button>
+  <button @click="increment">Count is: {{ count }}</button>
 </template>
 
 <style scoped>
