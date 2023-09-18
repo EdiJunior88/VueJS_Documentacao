9caf5a2c4f99e9c3011e19805531a951370d0a34
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-18 18:23:19 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,12 @@
 <script setup>
-const url = "https://www.w3schools.com/vue/img_beach3.jpg";
-const fontSize = "2rem";
-const fontWeight = "bold";
-const color = "blue";
-const isButtonDisabled = true
+const objectOfAttrs = {
+  id: "container",
+  class: "wrapper",
+};
 </script>
 
 <template>
-  <p>Using v-bind in image: <img v-bind:src="url" /></p>
-  <p>
-    Using v-bind with abbreviation:
-    <span :style="{ fontSize, fontWeight, color }">Text Example</span>
-  </p>
-  <p>
-    Using v-bind with boolean attribute
-    <button :disabled="isButtonDisabled">Button disabled</button>
-  </p>
+  <div v-bind="objectOfAttrs">div > id: "container" / class: "wrapper"</div>
 </template>
 
 <style>
