7ad8c24cb532e91271eee1d0b102e1905e7fa103
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-18 22:28:56 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,17 +1,13 @@
 <script setup>
-const number = 50;
-let test = true;
-const YES = "YES";
-const NO = "NO";
-const message = "The quick brown fox jumps over the lazy dog.";
-let id = 10;
+function consoleLog(value) {
+  console.log(value);
+}
+
+const teste = 'Este é um título de múltiplas linhas'
 </script>
 
 <template>
-  <p>The number {{ number }} + 1 = {{ number + 1 }}</p>
-  <p :style="{ color: test ? 'red' : 'green' }">{{ test ? YES : NO }}</p>
-  <p>{{ message.split("").reverse().join("") }}</p>
-  <div :id="`list-${id}`"></div>
+  <span :title="teste"> {{ consoleLog("console.log print in your browser") }} </span>
 </template>
 
 <style></style>
