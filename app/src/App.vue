624ad7a1771c6e6cc522daefacca81302265a206
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-18 21:54:37 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,16 +1,17 @@
 <script setup>
-const objectOfAttrs = {
-  id: "container",
-  class: "wrapper",
-};
+const number = 50;
+let test = true;
+const YES = "YES";
+const NO = "NO";
+const message = "The quick brown fox jumps over the lazy dog.";
+let id = 10;
 </script>
 
 <template>
-  <div v-bind="objectOfAttrs">div > id: "container" / class: "wrapper"</div>
+  <p>The number {{ number }} + 1 = {{ number + 1 }}</p>
+  <p :style="{ color: test ? 'red' : 'green' }">{{ test ? YES : NO }}</p>
+  <p>{{ message.split("").reverse().join("") }}</p>
+  <div :id="`list-${id}`"></div>
 </template>
 
-<style>
-img {
-  width: 40%;
-}
-</style>
+<style></style>
