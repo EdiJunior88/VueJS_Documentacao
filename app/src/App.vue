ca49d6951c13017cc28ecddfaaf430b2310b810d
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-20 20:17:59 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo
    (ref())

--- app/src/App.vue
+++ app/src/App.vue
@@ -3,13 +3,15 @@ import { ref } from "vue";
 
 const count = ref(0);
 
-console.log(count); //{value: 0}
+/* console.log(count); //{value: 0}
 console.log(count.value); //0
 
 count.value++;
-console.log(count.value); //1
+console.log(count.value); //1 */
 </script>
 
-<template></template>
+<template>
+  <button @click="count++">{{ count }}</button>
+</template>
 
 <style></style>
