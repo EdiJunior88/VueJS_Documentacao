bda651724aa06924718aa784febe46ee385eb35f
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-21 20:58:38 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,10 +1,23 @@
 <script setup>
-import MyComponent from "./MyComponent.vue";
+import { reactive, ref } from "vue";
+
+const activeColor = ref("red");
+const fontSize = ref(30);
+
+//Frequentemente é uma boa ideia vincular a um objeto de estilo diretamente
+//para que o modelo de marcação esteja mais limpo:
+const styleObject = reactive({
+  color: "red",
+  fontSize: "13px",
+});
 </script>
 
 <template>
-  <!-- quando estivermos utilizando o componente -->
-  <MyComponent class="baz boo" />
+  <div :style="{ color: activeColor, fontSize: fontSize + 'px' }">:style</div>
+  <div :style="{ 'font-size': fontSize + 'px' }">
+    :style CSS em "kebab-case"
+  </div>
+  <div :style="styleObject">:style vincular a um objeto de estilo</div>
 </template>
 
 <style></style>
