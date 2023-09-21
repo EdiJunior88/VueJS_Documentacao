cda2670c2f6c4eaf770273def8e5473f1d09ceaf
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-20 21:23:33 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,15 +1,36 @@
 <script setup>
 import { reactive, ref } from "vue";
 
-const books = reactive([ref("Vue 3 Guide")]);
-// neste caso precisas de `.value`
-console.log(books[0].value);
+//No exemplo abaixo, count e object são propriedades de alto nível, mas object.id não:
+const count = ref(0);
+const object = { id: ref(1) };
 
-const map = reactive(new Map([["count", ref(0)]]));
-// neste caso precisas de `.value`
-console.log(map.get("count").value);
+//Para corrigir isto, podemos desestruturar id à uma propriedade de alto nível:
+const { id } = object;
 </script>
 
-<template></template>
+<template>
+  <!-- Portanto, esta expressão funciona como esperado: -->
+  {{ count + 1 }}
+
+  <br />
+
+  <!-- ...enquanto isto NÃO: -->
+  {{ object.id + 1 }}
+
+  <br />
+
+  {{ id + 1 }}
+
+  <br />
+
+  <!-- Um outra coisa à notar é que uma referência é desembrulhada 
+    se for o valor avaliado final duma interpolação de texto 
+    (por exemplo, um marcador {{ }}, então o seguinte exemplo desenhará 1) -->
+  {{ object.id }}
+
+  <!-- Isto é apenas um funcionalidade de conveniência da interpolação de texto 
+  e é equivalente ao {{ object.id.value }}. -->
+</template>
 
 <style></style>
