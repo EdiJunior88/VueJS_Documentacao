10ee9eeed7bcd01291521f17f4facdd5d3811197
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-20 21:13:37 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,23 +1,21 @@
 <script setup>
-let state = reactive({ count: 0 });
+import { reactive, ref } from "vue";
 
-// a referência acima ({ count: 0 }) não está mais a ser rastreada
-// (conexão da reatividade está perdida!)
-state = reactive({ count: 1 });
+const count = ref(0);
+const state = reactive({ count });
 
-/* --------------------------------------------------------------------- */
+console.log(state.count); //0
 
-const state2 = reactive({ count: 0 });
+state.count = 1;
+console.log(count.value); //1
 
-// `count` está desconectado da `state.count` quando desestruturada.
-let { count } = state2.count;
-// não afeta o estado original
-count++;
+const otherCount = ref(2)
 
-// a função recebe um número simples e
-// não será capaz de rastrear as mudanças para `state.count`
-// temos de passar o objeto inteiro para reter a reatividade
-callSomeFunction(state2.count);
+state.count = otherCount
+console.log(state.count) //2;
+
+// a referência original agora está desconectada da `state.count`
+console.log(count.value);
 </script>
 
 <template></template>
