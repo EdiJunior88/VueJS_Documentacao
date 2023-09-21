78ad6b1599a7b3f18395607e72595fb3318378b4
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-20 22:57:27 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos

--- app/src/App.vue
+++ app/src/App.vue
@@ -10,25 +10,30 @@ const author = reactive({
   ],
 });
 
-/* Neste ponto, o modelo de marcação está ficando um pouco desarrumado. 
-Nós precisamos olhar nele por um segundo antes de perceber que ela realiza 
-um cálculo dependendo de author.books. Mais importante, nós provavelmente 
-não queremos nos repetir se precisarmos incluir este cálculo no modelo de 
+/* Neste ponto, o modelo de marcação está ficando um pouco desarrumado.
+Nós precisamos olhar nele por um segundo antes de perceber que ela realiza
+um cálculo dependendo de author.books. Mais importante, nós provavelmente
+não queremos nos repetir se precisarmos incluir este cálculo no modelo de
 marcação mais de uma vez.
 
-É por isto que para lógica complexa que inclui dados reativos, é recomendado 
+É por isto que para lógica complexa que inclui dados reativos, é recomendado
 utilizar uma propriedade computada. */
 
 // uma referência computada
 const publishedBooksMessage = computed(() => {
   return author.books.length > 0 ? "YES" : "NO";
 });
+
+const now = computed(() => Date.now());
 </script>
 
 <template>
   <!-- E quisermos exibir mensagens diferentes caso o author tiver alguns livros ou não: -->
   <p>Has published books:</p>
   <span>{{ publishedBooksMessage }}</span>
+
+  <p>Date update</p>
+  <span>{{ now }}</span>
 </template>
 
 <style></style>
