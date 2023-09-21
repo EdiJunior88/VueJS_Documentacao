b283a62879c99be88ba5cd949eaa21bd00893c73
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-20 21:06:17 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,25 @@
 <script setup>
 import { reactive } from "vue";
 
-const state = reactive({ count: 0 });
+const raw = {};
+const proxy = reactive(raw);
+
+// a delegação NÃO é igual ao original.
+console.log(proxy === raw); //false
+
+// a chamada de reactive() sobre o mesmo objeto retorna a mesma delegação
+console.log(reactive(raw) === proxy); //true
+
+// a chamada de reactive() sobre uma delegação retorna a si mesma
+console.log(reactive(proxy) === proxy); //true
+
+const proxy2 = reactive({});
+
+const raw2 = {};
+proxy2.nested = raw;
+console.log(proxy2.nested === raw); //false
 </script>
 
-<template>
-  <button @click="state.count++">{{ state.count }}</button>
-</template>
+<template></template>
 
 <style></style>
