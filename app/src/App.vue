d0740205cade88368165c1292c050f295baa1609
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-21 23:15:59 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,9 +1,33 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const awesome = ref(true);
+const ok = true;
+</script>
 
 <template>
-  <div :style="{ display: ['-webkit-box', '-ms-flexbox', 'flex'] }">
-    arranjo de vários valores (prefixados)
-  </div>
+  <button @click="awesome = !awesome">Toggle</button>
+
+  <h1 v-if="awesome">Vue is Awesome!</h1>
+  <h1 v-else>Oh no 😢</h1>
+
+  <!-- ************************************************ -->
+
+  <div v-if="type === 'A'">A</div>
+
+  <div v-else-if="type === 'B'">B</div>
+
+  <div v-else-if="type === 'C'">C</div>
+
+  <div v-else>Not A/B/C</div>
+
+  <!-- ************************************************ -->
+
+  <template v-if="ok">
+    <h1>Title</h1>
+    <p>Paragraph 1</p>
+    <p>Paragraph 2</p>
+  </template>
 </template>
 
 <style></style>
