f17260a84130ca8994604ba687bcd1b1fe91a471
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-26 20:02:18 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,11 +1,43 @@
 <script setup>
 import { ref } from "vue";
 
-const ok = ref(true);
+const items = ref([
+  {
+    message: "Foo",
+  },
+  {
+    message: "Bar",
+  },
+]);
+
+const parentMessage = ref("Parent");
 </script>
 
 <template>
-  <h1 v-show="ok">Hello!</h1>
+  <li v-for="(item, index) in items">
+    {{ parentMessage }} - {{ index }} - {{ item.message }}
+  </li>
+
+  <!-- 
+    Dentro do escopo de v-for, as expressões do modelo de marcação tem 
+    acesso a todas propriedades do escopo do pai. Além disto, a v-for 
+    também suporta um segundo pseudônimo opcional para índice do item atual:
+   -->
+
+  <!-- 
+    O escopo de variável da v-for é similar ao seguinte código de JavaScript:
+
+    const parentMessage = 'Parent'
+    const items = [
+      /* ... */
+    ]
+
+    items.forEach((item, index) => {
+      // tem acesso ao `parentMessage` do escopo externo 
+      // mas `item` e `index` apenas estão disponíveis aqui
+      console.log(parentMessage, item.message, index)
+    })
+    -->
 </template>
 
 <style></style>
