c3875139a9ea2c07c4afce34ead86ea2ecd2d9aa
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-26 20:08:59 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for
    -- v-for com um Objeto
    -- v-for com um Limite

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,38 +1,9 @@
-<script setup>
-import { reactive } from "vue";
-
-const myObject = reactive({
-  title: "How to do lists in Vue",
-  author: "Jane Doe",
-  publishedAt: "2016-04-10",
-});
-</script>
+<script setup></script>
 
 <template>
-  <ul>
-    <li v-for="value in myObject">
-      {{ value }}
-    </li>
-  </ul>
-
-  <!-- 
-    Tu também podes fornecer um segundo pseudônimo 
-    para o nome da propriedade (também conhecido como chave):
-   -->
-
-  <ul>
-    <li v-for="(value, key) in myObject">{{ key }}: {{ value }}</li>
-  </ul>
-
-  <!-- 
-    E um outro para o índice:
-   -->
-
-  <ul>
-    <li v-for="(value, key, index) in myObject">
-      {{ index }}. {{ key }}: {{ value }}
-    </li>
-  </ul>
+  <span v-for="n in 10">
+    {{ n }}
+  </span>
 </template>
 
 <style></style>
