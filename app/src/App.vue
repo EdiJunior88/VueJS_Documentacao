044d5385d6ab213588a63954025fa8ac8af92920
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-27 20:43:35 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for
    -- v-for com um Objeto
    -- v-for com um Limite
    -- v-for no <template>
    -- v-for com v-if
    -- Mantendo o Estado com key
    -- v-for com um Componente
    -- Detecção de Mudança de Arranjo
    -- Exibindo Resultados Filtrados ou Organizados
    
    - Manipulação de Evento
    -- Manipuladores Em Linha
    -- Manipuladores de Método
    -- Chamando Métodos em Manipuladores Em Linha
    -- Acessando Argumento de Evento nos Manipuladores Em Linha
    -- Modificadores de Evento
    -- Modificadores de Tecla
    
    - Vinculações de Entrada de Formulário
    -- Uso Básico

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,30 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+
+const message = ref("");
+</script>
 
 <template>
-  <!-- só chama `vm.submit()` quando a `tecla` pressionada for `Enter` -->
-  <input @keyup.enter="submit" />
-
-  <!-- 
-    Tu podes utilizar diretamente quaisquer nomes de tecla válidos 
-    expostos através de KeyboardEvent.key como modificadores 
-    convertendo-os para "kebab-case".
-   -->
-  <input @keyup.page-down="onPageDown" />
-
-  <!-- Alt + Enter -->
-  <input @keyup.alt.enter="clear" />
-
-  <!-- Ctrl + Click -->
-  <div @click.ctrl="doSomething">Do something</div>
-
-  <!-- isto disparará mesmo se Alt ou Shift também for pressionado -->
-  <button @click.ctrl="onClick">A</button>
-
-  <!-- isto só disparará quando Ctrl e não outras teclas for pressionado -->
-  <button @click.ctrl.exact="onCtrlClick">A</button>
-
-  <!-- isto só disparará quando nenhum dos modificadores do sistema for pressionado -->
-  <button @click.exact="onClick">A</button>
+  <p>Message is: {{ message }}</p>
+  <input v-model="message" placeholder="edit me" />
 </template>
 
 <style></style>
