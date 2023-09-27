6a3483731e6b2ddc8f1ae064df770c87b74499e6
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-26 22:52:19 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for
    -- v-for com um Objeto
    -- v-for com um Limite
    -- v-for no <template>
    -- v-for com v-if
    -- Mantendo o Estado com key
    -- v-for com um Componente
    -- Detecção de Mudança de Arranjo
    -- Exibindo Resultados Filtrados ou Organizados

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,34 @@
 <script setup>
-import { ref } from "vue";
-import MyComponent from "./MyComponent.vue";
+import { computed, ref } from "vue";
 
-const items = ref([
-  { name: "Edinaldo" },
-  { name: "Rozana" },
-  { name: "Marinalva" },
-  { name: "Ariclênio" },
-  { name: "Persivaldo" },
+const numbers = ref([1, 2, 3, 4, 5]);
+
+const sets = ref([
+  [1, 2, 3, 4, 5],
+  [6, 7, 8, 9, 10],
 ]);
+
+function even(numbers) {
+  return numbers.filter((number) => number % 2 === 0);
+}
+
+const eventNumbers = computed(() => {
+  return numbers.value.filter((n) => n % 2 === 0);
+});
 </script>
 
 <template>
-  <MyComponent
-    v-for="(item, index) in items"
-    :name="item.name"
-    :index="index" />
+  <li v-for="n in eventNumbers">{{ n }}</li>
+
+  <br />
+
+  <!-- Em situações onde as propriedades computadas não são viáveis 
+    (por exemplo, dentro de laços v-for encaixados), podes utilizar um método: 
+  -->
+
+  <ul v-for="numbers in sets">
+    <li v-for="n in even(numbers)">{{ n }}</li>
+  </ul>
 </template>
 
 <style></style>
