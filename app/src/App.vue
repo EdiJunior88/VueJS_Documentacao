bcbd0390434a80055bd330be78a9413be393295b
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-26 23:05:03 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for
    -- v-for com um Objeto
    -- v-for com um Limite
    -- v-for no <template>
    -- v-for com v-if
    -- Mantendo o Estado com key
    -- v-for com um Componente
    -- Detecção de Mudança de Arranjo
    -- Exibindo Resultados Filtrados ou Organizados
    
    - Manipulação de Evento
    -- Manipuladores Em Linha
    -- Manipuladores de Método
    -- Chamando Métodos em Manipuladores Em Linha
    -- Acessando Argumento de Evento nos Manipuladores Em Linha

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,12 +1,21 @@
 <script setup>
-function say(message) {
+// agora temos acesso ao evento nativo
+function warn(message, event) {
+  if (event) {
+    event.preventDefault();
+  }
   alert(message);
 }
 </script>
 
 <template>
-  <button @click="say('hello')">Say Hello</button>
-  <button @click="say('bye')">Say Bye</button>
+  <!-- utilizando a variável especial $event -->
+  <button @click="warn('Form cannot be submitted yet.', $event)">Submit</button>
+
+  <!-- utilizando a função em flecha em linha -->
+  <button @click="(event) => warn('Form cannot be submitted yet.')">
+    Submit
+  </button>
 </template>
 
 <style></style>
