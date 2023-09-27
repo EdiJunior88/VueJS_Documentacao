cf92b50309d7541a43326c7e473324b33118f004
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-26 23:06:06 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for
    -- v-for com um Objeto
    -- v-for com um Limite
    -- v-for no <template>
    -- v-for com v-if
    -- Mantendo o Estado com key
    -- v-for com um Componente
    -- Detecção de Mudança de Arranjo
    -- Exibindo Resultados Filtrados ou Organizados
    
    - Manipulação de Evento
    -- Manipuladores Em Linha
    -- Manipuladores de Método
    -- Chamando Métodos em Manipuladores Em Linha
    -- Acessando Argumento de Evento nos Manipuladores Em Linha
    -- Modificadores de Evento

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,33 @@
-<script setup>
-// agora temos acesso ao evento nativo
-function warn(message, event) {
-  if (event) {
-    event.preventDefault();
-  }
-  alert(message);
-}
-</script>
+<script setup></script>
 
 <template>
-  <!-- utilizando a variável especial $event -->
-  <button @click="warn('Form cannot be submitted yet.', $event)">Submit</button>
+  <!-- o propagação do evento de clique será interrompida -->
+  <a @click.stop="doThis"></a>
 
-  <!-- utilizando a função em flecha em linha -->
-  <button @click="(event) => warn('Form cannot be submitted yet.')">
-    Submit
-  </button>
+  <!-- o evento de submeter já não recarregará a página -->
+  <form @submit.prevent="onSubmit"></form>
+
+  <!-- modificadores podem ser encadeados -->
+  <a @click.stop.prevent="doThat"></a>
+
+  <!-- apenas o modificador -->
+  <form @submit.prevent></form>
+
+  <!-- apenas aciona o manipulador se `event.target` for o próprio elemento -->
+  <!-- por exemplo, não um elemento filho -->
+  <div @click.self="doThat">...</div>
+
+  <!-- utilize o modo de captura quando estiveres adicionando o ouvinte de evento -->
+  <!-- por exemplo, um evento mirando um elemento interno é manipulado aqui antes de ser manipulado por aquele elemento -->
+  <div @click.capture="doThis">...</div>
+
+  <!-- o evento de clique será acionado no máximo uma vez -->
+  <a @click.once="doThis"></a>
+
+  <!-- o comportamento padrão do evento de deslocamento (deslocando) acontecerá -->
+  <!-- imediatamente, no lugar de esperar o `onScroll` terminar  -->
+  <!-- neste caso ele contém `event.preventDefault()`                -->
+  <div @scroll.passive="onScroll">...</div>
 </template>
 
 <style></style>
