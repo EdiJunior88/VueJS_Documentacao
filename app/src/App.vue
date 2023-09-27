d94262a4941daba16cd3ee251fa4b62c80e7fba2
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-27 20:57:53 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for
    -- v-for com um Objeto
    -- v-for com um Limite
    -- v-for no <template>
    -- v-for com v-if
    -- Mantendo o Estado com key
    -- v-for com um Componente
    -- Detecção de Mudança de Arranjo
    -- Exibindo Resultados Filtrados ou Organizados
    
    - Manipulação de Evento
    -- Manipuladores Em Linha
    -- Manipuladores de Método
    -- Chamando Métodos em Manipuladores Em Linha
    -- Acessando Argumento de Evento nos Manipuladores Em Linha
    -- Modificadores de Evento
    -- Modificadores de Tecla
    
    - Vinculações de Entrada de Formulário
    -- Uso Básico
    (Texto de Várias Linhas (<textarea></textarea>) /
    Caixa de Confirmação (<input type="checkbox" /> /
    Rádio (<input type="radio" /> /)

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,33 +1,17 @@
 <script setup>
 import { ref } from "vue";
 
-const checked = ref(true);
-
-/* 
-  Nós também podemos vincular várias caixas de confirmação 
-  ao mesmo valor de arranjo ou conjunto: 
-*/
-const checkedNames = ref([]);
+const picked = ref("One");
 </script>
 
 <template>
-  <input type="checkbox" id="checkbox" v-model="checked" />
-  <label for="checkbox">{{ checked }}</label>
+  <div>Picked: {{ picked }}</div>
 
-  <!-- 
-    Neste caso, o arranjo checkedNames sempre conterá 
-    os valores das caixas confirmadas atualmente.
-   -->
-  <div>Checked names: {{ checkedNames }}</div>
+  <input type="radio" id="one" value="One" v-model="picked" />
+  <label for="one">One</label>
 
-  <input type="checkbox" id="jack" value="jack" v-model="checkedNames" />
-  <label for="jack">Jack</label>
-
-  <input type="checkbox" id="john" value="john" v-model="checkedNames" />
-  <label for="john">John</label>
-
-  <input type="checkbox" id="mike" value="mike" v-model="checkedNames" />
-  <label for="mike">Mike</label>
+  <input type="radio" id="two" value="Two" v-model="picked" />
+  <label for="two">Two</label>
 </template>
 
 <style></style>
