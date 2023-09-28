2d785b178b3598e97b68b924b27ffc97bbbbff06
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-28 11:16:34 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for
    -- v-for com um Objeto
    -- v-for com um Limite
    -- v-for no <template>
    -- v-for com v-if
    -- Mantendo o Estado com key
    -- v-for com um Componente
    -- Detecção de Mudança de Arranjo
    -- Exibindo Resultados Filtrados ou Organizados
    
    - Manipulação de Evento
    -- Manipuladores Em Linha
    -- Manipuladores de Método
    -- Chamando Métodos em Manipuladores Em Linha
    -- Acessando Argumento de Evento nos Manipuladores Em Linha
    -- Modificadores de Evento
    -- Modificadores de Tecla
    
    - Vinculações de Entrada de Formulário
    -- Uso Básico
    (Texto de Várias Linhas (<textarea></textarea>) /
    Caixa de Confirmação (<input type="checkbox" />) /
    Rádio (<input type="radio" />) /
    Seleção (<select></select>) /)
    -- Vinculações de Valor
    (Caixa de Confirmação (<input type="checkbox" />) /
    Rádio (<input type="radio" />) /
    Opções de Seleção (<option></option>))
    
    - Modificadores
    -- .lazy
    -- .number
    -- .trim
    
    - v-model com Componentes
    
    - Gatilhos do Ciclo de Vida
    -- Registando Gatilhos do Ciclo de Vida
    -- Diagrama do Ciclo de Vida
    
    - Observadores
    -- Exemplo Básico (Observar Tipos de Fonte)
    -- Observadores Profundos
    -- Observadores Incansáveis
    -- watchEffect()
    -- Tempo de Fluxo de Resposta
    -- Parando um Observador
    
    - Referências do Modelo de Marcação
    -- Acessando as Referências
    -- Referências dentro de v-for
    -- Referências de Função
    -- Referência no Componente
    
    - Fundamentos de Componentes
    -- Definindo um Componente
    -- Utilizando um Componente
    -- Passando Propriedades
    -- Ouvindo Eventos

--- app/src/BlogPost.vue
+++ app/src/BlogPost.vue
@@ -1,9 +1,16 @@
 <script setup>
 defineProps(["title"]);
+
+/* 
+Nós podemos opcionalmente declarar eventos emitidos utilizando a macro defineEmits:
+*/
+const emit = defineEmits(["enlarge-text"]);
 </script>
 
 <template>
   <h4>{{ title }}</h4>
+  <button @click="$emit('enlarge-text')">Enlarge text</button>
+  <button @click="emit('enlarge-text')">Enlarge Text With defineEmits</button>
 </template>
 
 <style lang="css" scoped></style>
