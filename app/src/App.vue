515fb99306491d621b690d0fce33ee39620f3fcd
EdiJunior88 <er.moura88@yahoo.com.br>
2023-09-27 21:56:14 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for
    -- v-for com um Objeto
    -- v-for com um Limite
    -- v-for no <template>
    -- v-for com v-if
    -- Mantendo o Estado com key
    -- v-for com um Componente
    -- Detecção de Mudança de Arranjo
    -- Exibindo Resultados Filtrados ou Organizados
    
    - Manipulação de Evento
    -- Manipuladores Em Linha
    -- Manipuladores de Método
    -- Chamando Métodos em Manipuladores Em Linha
    -- Acessando Argumento de Evento nos Manipuladores Em Linha
    -- Modificadores de Evento
    -- Modificadores de Tecla
    
    - Vinculações de Entrada de Formulário
    -- Uso Básico
    (Texto de Várias Linhas (<textarea></textarea>) /
    Caixa de Confirmação (<input type="checkbox" />) /
    Rádio (<input type="radio" />) /
    Seleção (<select></select>) /)
    -- Vinculações de Valor
    (Caixa de Confirmação (<input type="checkbox" />) /
    Rádio (<input type="radio" />) /
    Opções de Seleção (<option></option>))

--- app/src/App.vue
+++ app/src/App.vue
@@ -2,13 +2,15 @@
 
 <template>
   <!-- 
-    O pick será definido para o valor de first quando 
-    a primeira entrada de rádio for confirmada, e 
-    definida para o valor de second quando o segundo 
-    for confirmado.
+    A v-model também suporta vinculações de valor de valores 
+    que não são sequência de caracteres! No exemplo acima, 
+    quando a opção é selecionada, selected será definida 
+    para o valor literal de objeto de { number: 123 }.
    -->
-  <input type="radio" v-model="pick" :value="first" />
-  <input type="radio" v-model="pick" :value="second" />
+  <select v-model="selected">
+    <!-- inline object literal -->
+    <option :value="{ number: 123 }">123</option>
+  </select>
 </template>
 
 <style lang="scss" scoped></style>
