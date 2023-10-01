7b0ee0548a91b78efa4bd2654545d3dbe868d9f9
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-01 18:19:15 -0300

    - Começar -- Introdução O que a Vue é? A Abstração Progressiva Componentes de Ficheiro Único Estilos de API (API de Opções / API de Composição) Qual escolher? Continua Com Dúvidas? Escolha o Teu Caminho de Aprendizado
    -- Introdução Rápida
    Experimente a Vue Online
    Criando uma Aplicação de Vue
    Utilizando a Vue a partir da CDN
    Próximos Passos
    
    - Fundamentos
    -- Criando uma Aplicação de Vue
    A instância de aplicação
    O Componente de Raiz
    Montando a Aplicação
    Configurações da Aplicação
    Várias instâncias de aplicação
    
    -- Sintaxe do Modelo de Marcação
    Interpolação de Texto
    HTML Puro
    Vinculações de Atributo (Abreviação / Atributos Booleano / Vincular Vários Atributos Dinamicamente)
    Utilizando Expressões de JavaScript
    Apenas Expressões
    Chamando Funções
    Acesso Global Restrito
    Diretivas
    Argumentos
    Argumentos Dinâmicos (Restrições de Valor do Argumento Dinâmico /
    Restrições de Sintaxe do Argumento Dinâmico /
    Modificadores)
    
    - Fundamentos de Reatividade
    -- Declarando Estado Reativo (ref() / <script setup>)
    Porquê Referências?
    Reatividade Profunda
    Tempo de Atualização do DOM (reactive() / Delegação Reativa vs. Original
    / Limitações da reactive())
    -- Detalhes Adicionais do Desembrulhamento da Referência (Como Propriedade de Objeto Reativo
    / Advertências nos Vetores e Coleções
    / Advertências Quando Desembrulhamos nos Modelos de Marcação)
    
    - Propriedades Computadas
    -- Exemplo Básico
    -- Armazenamento de consulta imediata de computado vs Métodos
    -- Propriedades Computadas Graváveis
    -- Boas Práticas
    
    - Vinculações de Classe e Estilo
    -- Vinculando Classes de HTML (Vinculando aos Objetos
    / Vinculando aos Arranjos / Com Componentes)
    -- Vinculando Estilos Em Linha (Vinculando aos Objetos / Vinculando aos Arranjos
    / Prefixação Automática / Vários Valores)
    
    - Interpretação Condicional
    -- v-if
    -- v-else
    -- v-else-if
    -- v-if no <template>
    -- v-show
    -- v-if com v-for
    
    - Interpretação de Lista
    -- v-for
    -- v-for com um Objeto
    -- v-for com um Limite
    -- v-for no <template>
    -- v-for com v-if
    -- Mantendo o Estado com key
    -- v-for com um Componente
    -- Detecção de Mudança de Arranjo
    -- Exibindo Resultados Filtrados ou Organizados
    
    - Manipulação de Evento
    -- Manipuladores Em Linha
    -- Manipuladores de Método
    -- Chamando Métodos em Manipuladores Em Linha
    -- Acessando Argumento de Evento nos Manipuladores Em Linha
    -- Modificadores de Evento
    -- Modificadores de Tecla
    
    - Vinculações de Entrada de Formulário
    -- Uso Básico
    (Texto de Várias Linhas (<textarea></textarea>) /
    Caixa de Confirmação (<input type="checkbox" />) /
    Rádio (<input type="radio" />) /
    Seleção (<select></select>) /)
    -- Vinculações de Valor
    (Caixa de Confirmação (<input type="checkbox" />) /
    Rádio (<input type="radio" />) /
    Opções de Seleção (<option></option>))
    
    - Modificadores
    -- .lazy
    -- .number
    -- .trim
    
    - v-model com Componentes
    
    - Gatilhos do Ciclo de Vida
    -- Registando Gatilhos do Ciclo de Vida
    -- Diagrama do Ciclo de Vida
    
    - Observadores
    -- Exemplo Básico (Observar Tipos de Fonte)
    -- Observadores Profundos
    -- Observadores Incansáveis
    -- watchEffect()
    -- Tempo de Fluxo de Resposta
    -- Parando um Observador
    
    - Referências do Modelo de Marcação
    -- Acessando as Referências
    -- Referências dentro de v-for
    -- Referências de Função
    -- Referência no Componente
    
    - Fundamentos de Componentes
    -- Definindo um Componente
    -- Utilizando um Componente
    -- Passando Propriedades
    -- Ouvindo Eventos
    -- Distribuição de Conteúdo com Ranhuras

---
+++ app/src/AlertBox.vue
@@ -0,0 +1,28 @@
+<script setup>
+/* 
+Como verás acima, utilizamos o <slot> como um espaço reservado 
+onde queremos que conteúdo esteja – e é isto. Terminamos!
+*/
+</script>
+
+<template>
+  <div class="alert-box">
+    <strong>Error</strong>
+    <br />
+    <slot />
+  </div>
+</template>
+
+<style lang="css" scoped>
+.alert-box {
+  color: #666;
+  border: 1px solid red;
+  border-radius: 4px;
+  padding: 20px;
+  background-color: #f8f8f8;
+}
+
+strong {
+  color: red;
+}
+</style>
--- app/src/App.vue
+++ app/src/App.vue
@@ -1,24 +1,9 @@
 <script setup>
-import { ref } from "vue";
-import BlogPost from "./BlogPost.vue";
-
-const posts = ref([
-  { id: 1, title: "My Journey with Vue" },
-  { id: 2, title: "Blogging with Vue" },
-  { id: 3, title: "Why Vue is so fun" },
-]);
-
-const postFontSize = ref(1);
+import AlertBox from "./AlertBox.vue";
 </script>
 
 <template>
-  <div :style="{ fontSize: postFontSize + 'em' }">
-    <BlogPost
-      v-for="post in posts"
-      :key="post.id"
-      :title="post.title"
-      @enlarge-text="postFontSize += 0.1" />
-  </div>
+  <AlertBox> Something bad happened. </AlertBox>
 </template>
 
 <style lang="css" scoped></style>
