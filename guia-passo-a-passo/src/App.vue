99c5eaec7d5b82191af9a9e7739bd29f59db68d8
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-03 20:33:57 -0300

    Guia
    - Começar
    - Interpretação Declarativa
    - Vinculação de Atributo
    - Ouvintes de Eventos
    - Vinculação de Formulário
    - Interpretação Condicional

--- guia-passo-a-passo/src/App.vue
+++ guia-passo-a-passo/src/App.vue
@@ -1,19 +1,17 @@
 <script setup>
 import { ref } from 'vue'
 
-const text = ref('')
+const awesome = ref(true)
 
-function onInput(e) {
-  text.value = e.target.value
+function toggle() {
+  awesome.value = !awesome.value
 }
 </script>
 
 <template>
-  <input :value="text" @input="onInput" placeholder="Type here" />
-  <p>{{ text }}</p>
-  <!-- ou -->
-  <!-- <input v-model="text" placeholder="Type here">
-  <p>{{ text }}</p> -->
+  <button @click="toggle">toggle</button>
+  <h1 v-if="awesome">Vue is awesome</h1>
+  <h1 v-else>Oh no 😢</h1>
 </template>
 
 <style lang="css" scoped></style>
