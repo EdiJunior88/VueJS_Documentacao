2926f8ea93c6330f53b4c98d580f59ba3f5d5217
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-03 20:03:55 -0300

    Guia
    - Começar
    - Interpretação Declarativa
    - Vinculação de Atributo
    - Ouvintes de Eventos

--- guia-passo-a-passo/src/App.vue
+++ guia-passo-a-passo/src/App.vue
@@ -1,15 +1,17 @@
 <script setup>
 import { ref } from 'vue'
 
-const titleClass = ref('title')
+const count = ref(0)
+
+function increment() {
+  //update component state
+  //atualize o estado do componente
+  count.value++
+}
 </script>
 
 <template>
-  <h1 :class="titleClass">Make me red</h1>
+  <button @click="increment">count is: {{ count }}</button>
 </template>
 
-<style lang="css" scoped>
-.title {
-  color: red;
-}
-</style>
+<style lang="css" scoped></style>
