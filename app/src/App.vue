024225043893853ad6d074aa203c6cc07227f408
Edivaldo Junior <er.moura88@yahoo.com.br>
2023-10-06 21:21:33 -0300

    Guia
    - Começar
    - Interpretação Declarativa
    - Vinculação de Atributo
    - Ouvintes de Eventos
    - Vinculação de Formulário
    - Interpretação Condicional
    - Interpretação de Lista
    - Propriedade Computada

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,13 +1,21 @@
 <script setup>
-import { ref } from "vue";
+import { computed, h, ref } from "vue";
 
 let id = 0;
+
 const newTodo = ref("");
+const hideCompleted = ref(false);
 const todos = ref([
   { id: id++, text: "Learn HTML" },
   { id: id++, text: "Learn JavaScript" },
   { id: id++, text: "Learn Vue" },
 ]);
+
+const filteredTodos = computed(() => {
+  // retorna afazeres `todos` filtrados com base nos
+  // `todos.value` & `hideCompleted.value`
+  return hideCompleted.value ? todos.value.filter((t) => !t.done) : todos.value;
+});
 
 function addTodo() {
   todos.value.push({ id: id++, text: newTodo.value });
@@ -26,11 +34,20 @@ function removeTodo(todo) {
   </form>
 
   <ul>
-    <li v-for="todo in todos" :key="todo.id">
-      {{ todo.text }}
+    <li v-for="todo in filteredTodos" :key="todo.id">
+      <input type="checkbox" v-model="todo.done" />
+      <span :class="{ done: todo.done }">{{ todo.text }}</span>
       <button @click="removeTodo(todo)">X</button>
     </li>
   </ul>
+
+  <button @click="hideCompleted = !hideCompleted">
+    {{ hideCompleted ? "Show all" : "Hide completed" }}
+  </button>
 </template>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.done {
+  text-decoration: line-through;
+}
+</style>
